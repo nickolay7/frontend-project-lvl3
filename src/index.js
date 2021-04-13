c5be383f831a1ff0b51d6fced980113662857f8c
Nickolay Semutkin <shkelal50@gmail.com>
2021-04-13 14:19:11 +0300

    added validator

--- src/index.js
+++ src/index.js
@@ -1,3 +1,39 @@
 import 'bootstrap';
+import * as yup from 'yup';
+import onChange from 'on-change';
 
-console.log('Hello Webpack!');
+const regexp = /^.+\.rss(\.xml)?$/;
+
+const rssSchema = yup.string().url().test(
+  'is-valid',
+  'url is not rss',
+  (value) => regexp.test(value),
+);
+
+const state = {
+  form: {
+    valid: true,
+  },
+};
+
+const form = document.querySelector('form');
+const input = document.querySelector('input[name="name"]');
+
+const watchedState = onChange(state, (path, value) => {
+  if (path === 'form.valid') {
+    if (value === false) {
+      input.classList.add('is-invalid');
+    }
+  }
+});
+
+form.addEventListener('submit', (e) => {
+  e.preventDefault();
+  const formData = new FormData(e.target);
+  const feed = formData.get('name');
+  // console.log(rssSchema.isValid(feed));
+  rssSchema.isValid(feed)
+    .then((res) => {
+    if(!res) watchedState.form.valid = false;
+  });
+});
