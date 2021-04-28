23358b52b54e98b9f6909af409516918f8895737
Nickolay Semutkin <shkelal50@gmail.com>
2021-04-28 20:04:55 +0300

    refactor

--- src/init.js
+++ src/init.js
@@ -177,44 +177,44 @@ const postsUpdate = (state, i18n) => {
     }).then(() => setTimeout(postsUpdate, 5000, state, i18n));
 };
 
-export default () => {
+export default async () => {
   const defaultLanguage = 'ru';
   const i18n = i18next.createInstance();
 
-  i18n.init({
+  await i18n.init({
     lng: defaultLanguage,
     debug: false,
     resources,
-  }).then(() => {
-    const state = {
-      lng: defaultLanguage,
-      form: {
-        error: '',
-      },
-      currentData: '',
-      urls: [],
-    };
+  });
 
-    const watchedState = onChange(state, (path, value) => {
-      switch (path) {
-        case 'form.error':
-          errorHandler(value, i18n);
-          break;
-        case 'currentData':
-          feedsRender(state.currentData, i18n);
-          postsRender(state.currentData, i18n);
-          postsUpdate(state, i18n);
-          break;
-        default:
-          break;
-      }
-    });
+  const state = {
+    lng: defaultLanguage,
+    form: {
+      error: '',
+    },
+    currentData: '',
+    urls: [],
+  };
 
-    form.addEventListener('submit', (e) => {
-      e.preventDefault();
-      const formData = new FormData(e.target);
-      const feed = formData.get('url');
-      isValidRss(feed, watchedState);
-    });
+  const watchedState = onChange(state, (path, value) => {
+    switch (path) {
+      case 'form.error':
+        errorHandler(value, i18n);
+        break;
+      case 'currentData':
+        feedsRender(state.currentData, i18n);
+        postsRender(state.currentData, i18n);
+        postsUpdate(state, i18n);
+        break;
+      default:
+        break;
+    }
+  });
+
+  form.addEventListener('submit', (e) => {
+    e.preventDefault();
+    const formData = new FormData(e.target);
+    const feed = formData.get('url');
+    isValidRss(feed, watchedState);
   });
 };
